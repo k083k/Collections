4cfcbaccd376996fbbf88bb20546f425968be6cf
Kwabena Frempong-Kore <kobekore@gmail.com>
2020-02-23 02:12:05 +0000

    Main.java: new commands

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Theater theater = new Theater("Banjo", 10, 10);
         theater.reserveSeat("J10");
-        theater.getSeats();
-
+        theater.cancelReservation("J10");
     }
 }
