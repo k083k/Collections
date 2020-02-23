742729e8b73fc68c8df069828fc1f976094cab8b
Kwabena Frempong-Kore <kobekore@gmail.com>
2020-02-23 01:50:57 +0000

    Initial Commit. Theater Project

---
+++ src/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theater theater = new Theater("Banjo", 10, 10);
+        theater.reserveSeat("J10");
+        theater.getSeats();
+
+    }
+}
